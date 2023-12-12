5afcaef7fcb3fea9b522a9c4ac374c791bc7279d
alavudheen.e <alavudheen.e@claysys.net>
2023-12-12 21:36:27 +0530

    Todo list updated project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
